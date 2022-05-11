00c12e9e2f35d9993dbcb679776e13aeac77f7db
Бирюкова Елена Витальевна <lenabir68@gmail.com>
2022-05-11 19:01:26 +0300

    Настройки Web

--- System/Web/TaskLibrary.Web/Settings.cs
+++ System/Web/TaskLibrary.Web/Settings.cs
@@ -2,11 +2,11 @@
 {
     public static class Settings
     {
-        //public static string ApiRoot = "http://localhost:20000/api";
+        public static string ApiRoot = "http://localhost:20000/api";
 
-        public static string ApiRoot = "http://host.docker.internal:20000/api";
-        //public static string IdentityRoot = "http://localhost:20001";
-        public static string IdentityRoot = "http://host.docker.internal:20001";
+       // public static string ApiRoot = "http://host.docker.internal:20000/api";
+        public static string IdentityRoot = "http://localhost:20001";
+        //public static string IdentityRoot = "http://host.docker.internal:20001";
         
         public static string ClientId = "frontend";
         public static string ClientSecret = "secret";
